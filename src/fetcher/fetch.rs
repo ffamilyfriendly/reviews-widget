dd0310eb9058dba87bdb6b104c6ac5a1e5717a37
ffamilyfriendly <me@familyfriendly.xyz>
2023-06-26 17:03:01 +0200

    adds: (bad) algo for ranking reviews by relevance

--- src/fetcher/fetch.rs
+++ src/fetcher/fetch.rs
@@ -1,6 +1,7 @@
-use std::{error::Error, collections::HashMap};
+use std::{error::Error, cmp::{max, min}, collections::{HashMap, HashSet}};
 use serde::{Deserialize, Serialize};
 use reqwest;
+use chrono::{DateTime, Datelike};
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Review {
@@ -9,7 +10,8 @@ pub struct Review {
     pub votes: u16,
     pub content: String,
     pub author_display_name: String,
-    pub author_profile_picture: String
+    pub author_profile_picture: String,
+    score_factor: u16
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
@@ -37,6 +39,43 @@ struct RawApiResponse {
     poster: RawApiPoster
 }
 
+fn calculate_score_factor( rev_data: &RawApiResponse ) -> u16 {
+
+    /*
+        TODO: I kinda wanna refractor this until the result is a float where a 1.0 means highest relevance
+        and 0.0 means zero relevance. I'm no good at these type of algos but I'll take a peep around the internet for inspiration
+
+        works decently for now
+    */
+
+    let mut score: u16 = 0;
+
+    // Create a HashSet with all the "words" (segments of text terminated by ' ')
+    let words: HashSet<&str> = rev_data.content.split(" ").collect();
+
+    // Decode the timestamp to an Arr [year, month, day]
+    let timestamp_arr: Vec<u32> = rev_data.timestamp[..10].split("-").map(|x| x.parse::<u32>().unwrap_or(2016)).collect();
+
+    // Get the number of days ago the review was submitted
+    let days_since: u16 = (|| {
+        let current_date: chrono::NaiveDate = chrono::UTC::now().naive_utc().date();
+        let review_date = chrono::NaiveDate::from_ymd_opt(timestamp_arr[0] as i32, timestamp_arr[1], timestamp_arr[2]).unwrap();
+        let delta = current_date - review_date;
+
+        return delta.num_days() as u16;
+    })();
+
+    // divide the len of unique words by 2 and return that OR 12 (whichever is smallest) as score
+    score += min(( words.len() as f32 / 2.0) as u16, 12);
+    score += min(max( 0,  365 - days_since as i32), 100) as u16;
+    score += rev_data.votes / 2;
+
+    // Dunno if I actually want to rank based on the rating
+    // as I want the widget to be fair but relevant
+    score += rev_data.score as u16;
+
+    score
+}
 
 async fn fetch_entity_reviews( id: String ) -> Result<ReviewsListing, Box<dyn Error>> {
     
@@ -52,7 +91,7 @@ async fn fetch_entity_reviews( id: String ) -> Result<ReviewsListing, Box<dyn Er
                 reviews_count += 1;
                 reviews_score_total += x.score as u16;
 
-                Review { rating: ((x.score as f32 / 100.0) * 5.0) as u8, timestamp: x.timestamp[..10].to_string(), votes: x.votes, content: x.content, author_display_name: x.poster.username, author_profile_picture: x.poster.avatar.unwrap_or("".to_string()) }
+                Review { score_factor: calculate_score_factor(&x), rating: ((x.score as f32 / 100.0) * 5.0) as u8, timestamp: x.timestamp[..10].to_string(), votes: x.votes, content: x.content, author_display_name: x.poster.username, author_profile_picture: x.poster.avatar.unwrap_or("".to_string()) }
             }).collect();
         if result.len() == 0 {
             break;
@@ -60,7 +99,7 @@ async fn fetch_entity_reviews( id: String ) -> Result<ReviewsListing, Box<dyn Er
         reviews_list.append(&mut result)
     }
 
-    reviews_list.sort_by(| b, a | a.timestamp.cmp(&b.timestamp).then(a.rating.cmp(&b.rating)) );
+    reviews_list.sort_by(| b, a | a.score_factor.cmp(&b.score_factor) );
 
     Ok(ReviewsListing {
         id: id,
